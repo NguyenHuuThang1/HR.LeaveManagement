cde0c04af4d9048291f89ab50fb50f872273f228
NguyenHuuThang1 <nhthanggg1007@gmail.com>
2026-06-03 09:50:27 +0700

    init domain, application

---
+++ HR.LeaveManagement.Application/Features/LeaveRequests/Handlers/Queries/GetLeaveRequestDetailRequestHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs.LeaveRequest;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequests.Handlers.Queries
+{
+    public class GetLeaveRequestDetailRequestHandler : IRequestHandler<GetLeaveRequestDetailRequest, LeaveRequestDto>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly IMapper _mapper;
+        private readonly IUserService _userService;
+
+        public GetLeaveRequestDetailRequestHandler(ILeaveRequestRepository leaveRequestRepository,
+            IMapper mapper,
+            IUserService userService)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+            this._userService = userService;
+        }
+        public async Task<LeaveRequestDto> Handle(GetLeaveRequestDetailRequest request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = _mapper.Map<LeaveRequestDto>(await _leaveRequestRepository.GetLeaveRequestWithDetails(request.Id));
+            leaveRequest.Employee = await _userService.GetEmployee(leaveRequest.RequestingEmployeeId);
+            return leaveRequest;
+        }
+    }
+}
